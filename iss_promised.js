53a8df7c4a28ec256125a1d4690fdc90b8c4f92e
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-18 17:02:01 +0000

    implemented prmoises to refactor our callback hell

--- iss_promised.js
+++ iss_promised.js
@@ -1,9 +1,34 @@
 const request = require('request-promise-native');
 
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
 const fetchMyIP = function() {
   return request(`https://api.ipify.org/?format=json`);
 };
 
+/*
+ * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(coords) {
+  const { latitude, longitude } = JSON.parse(coords);
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+
+module.exports = { nextISSTimesForMyLocation };
 
 
-module.exports = { fetchMyIP };
